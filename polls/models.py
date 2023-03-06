d19d1e3aa927973a7f5646281738ad551269b1be
leralime <limelera72@gmail.com>
2023-03-06 19:31:59 +0300

    сделали модели

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name="choices", on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.question.question_text} {self.choice_text}: {self.votes}"
+
+    # TODO к вопросу добавить 5 вопрос с 3мя вариантами ответов
+    # TODO построить находящий этот вопрос Question.objects.filter(....)
